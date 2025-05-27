a45dabec85e391b356aca252b217a38666dccf93
Emdiso <897482672@qq.com>
2025-05-27 20:11:05 +0800

    fix

--- music_recommendation/settings.py
+++ music_recommendation/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.getenv("DB_NAME", "music_recommendation"),
         "USER": os.getenv("DB_USER", "root"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "123717"),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
         "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", "3306"),
         "OPTIONS": {
